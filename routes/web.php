f660de9cb820717c4544dc54316b724c052b9867
glhwbsn <galehwibisono270101@gmail.com>
2026-04-08 17:00:42 +0700

    Mencoba menggunakan Route

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,22 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
-//Route::get('/', function () {
-  //  return view('welcome')
-//});
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+Route::get('/user/{id}', function ($id) {
+    return 'User dengan ID' . $id;
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function (){
+        return 'Admin Dashboard';
+    });
+    Route::get('/users', function () {
+        return 'Admin Users';
+    });
+});
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/contact', [HomeController::class, 'contact']);
