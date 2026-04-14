f77849ed09cfacb7b02dcf9720b3c56738637d74
glhwbsn <galehwibisono270101@gmail.com>
2026-04-14 08:02:24 +0700

    Menambahkan beberapa perintah dari tugas P5

--- resources/views/galeh_view.blade.php
+++ resources/views/galeh_view.blade.php
@@ -1,4 +1,14 @@
 <html>
+    <head>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <link rel="stylesheet" href="{{ asset('styles/style_galeh.css') }}">
+    <h1>Ini Judul Merah</h1>
+    <img src="{{ asset('images/galeh_laravel1.png')}}" alt="">
+    <img src="{{ asset('images/galeh_laravel2.png')}}" alt="">
+    <div class="bg-blue-500 p-4 m-4 rounded-lg text-white">
+        Ini pakai Tailwind
+    </div>
 <table>
     <thead>
         <tr>
